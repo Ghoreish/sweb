86a9e66370574294222854bd3211ff1aaaed1dac
ghoreish <sghoreish@gmail.com>
2019-06-03 22:10:57 +0430

    Now you can run several hosts

--- web_server.py
+++ web_server.py
@@ -2,33 +2,52 @@ import socket,_thread,os
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(("0.0.0.0",80))
 s.listen(10)
-def greq(d):#get directory of a http request
+def greq(d):
+    # get directory of a http request
     d=str(d)
     if 'GET' == d[2:5]:
         d=d.split('GET /')[1]
         d=d.split(' HTTP')[0]
-        print(d)
         return d
+    else:
+        return ""
+def ghost(d):
+    d = str(d)
+    if '\\r\\nHost: ' in d:
+        d = d.split('\\r\\nHost: ')[1]
+        d = d.split('\\r\\n')[0]
+        return d+"/"
     else:
         return ""
 def conn(x):
     data=x.recv(10240)
     req=greq(data)
+    hostn=ghost(data)
+    hostlist=os.listdir()
+    hostlist.remove("files")
+    hostlist.remove("pages")
+    if hostn[:-1] not in os.listdir():
+        hostn=""
     if req=='':
         req='main.html'
-    if req in os.listdir('pages') and req.split(".")[-1] == "html":#pages dir
-        res=open("pages/"+req,"rb").read()
+    if req in os.listdir(hostn+'pages') and req.split(".")[-1] == "html":
+        # pages dir
+        res=open(hostn+"pages/"+req,"rb").read()
         x.send(b'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: '+str(len(res)).encode()+b'\r\n\r\n'+res+b'\r\n\r\n')
-    elif req in os.listdir('files') and req.split(".")[-1] != "html":#files dir
-        tdic = {"png": "image", "jpg": "image", "jpeg": "image", "x-icon": "image"}#you can add other types here
-        res = open("files/" + req, "rb").read()
+    elif req in os.listdir(hostn+'files') and req.split(".")[-1] != "html":
+        # files dir
+        # you can add other types here
+        tdic = {"png": "image", "jpg": "image", "jpeg": "image", "x-icon": "image"}
+        res = open(hostn+"files/" + req, "rb").read()
         if req.split(".")[-1] == "ico":
             req = req[:-3] + "x-icon"
         x.send(b'HTTP/1.1 200 OK\r\nContent-Type: ' + tdic[req.split(".")[-1]].encode() + b'/' + req.split(".")[-1].encode() + b'\r\nContent-Length: ' + str(len(res)).encode() + b'\r\n\r\n' + res + b'\r\n\r\n')
-    else:#not found page in pages dir
-        res =open("pages/notfound.html", "r").read()
+    else:
+        # not found page in pages dir
+        res =open(hostn+"pages/notfound.html", "r").read()
         x.send(b'HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\nContent-Length: ' + str(len(res)).encode() + b'\r\n\r\n' + res.encode() + b'\r\n\r\n')
     x.close()
 while True:
+    # look how simple it is :)
     a,b=s.accept()
-    _thread.start_new_thread(conn,(a,))#look how simple it is :)
+    _thread.start_new_thread(conn,(a,))
